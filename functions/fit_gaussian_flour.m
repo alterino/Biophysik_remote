6d390780e0e14fe822b1081df57b13a393cc9a9e
mjmarino <mjmarino317@gmail.com>
2017-05-17 16:53:02 +0200

    fixed bug in D2GaussFunction and cleaned params in fit_gaussian_flour

--- functions/fit_gaussian_flour.m
+++ functions/fit_gaussian_flour.m
@@ -3,8 +3,8 @@ function [x, resnorm, residual, exitflag] = fit_gaussian_flour( img, bw_img )
 %   Detailed explanation goes here
 
 dim = size(img, 1);
-x0 = [1,0,50,0,50,0];
-lb = [0,-dim/2,0,-dim/2,0,-pi/4];
+x0 = [1,0,50,0,50];
+lb = [0,-dim/2,0,-dim/2,0];
 ub = [realmax('double'),dim/2,(dim/2)^2,dim/2,(dim/2)^2];
 
 [X,Y] = meshgrid(-dim/2+.5:dim/2-.5);
