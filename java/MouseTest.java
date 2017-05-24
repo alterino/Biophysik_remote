e4d72956516014d93a467ea7b5922116430ddf5d
mjmarino317 <mjmarino317@gmail.com>
2017-05-24 02:24:40 +0200

    java fucking around

---
+++ java/MouseTest.java
@@ -0,0 +1,51 @@
+// MouseTest.java
+
+import java.awt.AWTEvent;
+import java.awt.MouseInfo;
+import java.awt.Toolkit;
+import java.awt.event.AWTEventListener;
+
+import javax.swing.JFrame;
+
+public class MouseTest{
+    
+    public MouseTest(){
+        setVersion(0);
+    }
+    
+    public static void main(String[] args){
+        Toolkit.getDefaultToolkit().addAWTEventListener(
+        new Listener(), AWTEvent.MOUSE_EVENT_MASK | AWTEvent.FOCUS_EVENT_MASK);
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+        
+        System.out.println("Hello World!");
+    }
+    
+    private static class Listener implements AWTEventListener {
+        public void eventDispatched(AWTEvent event) {
+            System.out.print(MouseInfo.getPointerInfo().getLocation() + " | ");
+            //System.out.println(event);
+        }
+    }
+    
+    public void setVersion(int aVersion){
+        if( aVersion < 0 ){
+            System.err.println("Improper version specified.");
+        }
+        else{
+            version = aVersion;
+        }
+    }
+    
+    public int getVersion(){
+        return version;
+    }
+    
+    
+    private int version;
+    public int x;
+    public int y;
+    
+}
