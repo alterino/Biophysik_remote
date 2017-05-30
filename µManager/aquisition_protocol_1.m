8b5472398f31ece3811601fc73846723b9c743fb
mjmarino317 <mjmarino317@gmail.com>
2017-05-30 14:26:15 +0200

    added a skeleton approach for image classification

--- µManager/aquisition_protocol_1.m
+++ µManager/aquisition_protocol_1.m
@@ -48,15 +48,15 @@ for idxFrame = 1:10
     for idxPos = 1:size(listCoordXY,1);
         set_xy_pos_micron(objMicMan,listCoordXY(idxPos,:)) %move stage
         pause(0)
-
+        
         %% DIC
         set_exposure_time(objMicMan,50) %[ms]
         [img{idxPos,1}(:,:,idxFrame),meta{idxPos,1}(idxFrame)] = snap_img_DIC(objMicMan);
         
-        %% Fluor        
+        %% Fluor
         %640nm
-%         set_exposure_time(objMicMan,500) %[ms]
-%         [img{idxPos,2}(:,:,idxFrame),meta{idxPos,2}(idxFrame)] = snap_img_fluorescence(objMicMan,640);
+        %         set_exposure_time(objMicMan,500) %[ms]
+        %         [img{idxPos,2}(:,:,idxFrame),meta{idxPos,2}(idxFrame)] = snap_img_fluorescence(objMicMan,640);
         
         set_exposure_time(objMicMan,30) %[ms]
         img{idxPos,2}(:,:,:,idxFrame) = mov_fluorescence(objMicMan,640,100);
@@ -65,3 +65,62 @@ for idxFrame = 1:10
     end
     pause(5*60)
 end
+
+
+%% image analysis
+% if( exist(img,1)==0 )
+%     img = imread('D:\OS_Biophysik\Microscopy\170523\DIC_170523_1537.tif');
+% end
+% placeholder for generating image stack
+% assumes a 2D image composed of 600x600 images
+img_stack = img_2D_to_img_stack(img, [600, 600] );
+% placeholder for generating gmm model or, alternatively, loading previous
+% model
+% gmm = generate_gmm_entropy();
+% load( **insert gmm file here** );
+load( 'gmm.mat' )
+bw_stack = zeros( size(img_stack) );
+
+for i = 1:size(img_stack)
+    if( max(max( img_stack(:,:,i) ) ) == 0 )
+        continue
+    else
+        [clustered_img, ~] = ...
+            cluster_img_entropy( img_stack(:,:,i), [], gmm, 9, 1000);
+        bw_stack(:,:,i) = (clustered_img > 1);
+        temp_perim = bwperim( bw_stack(:,:,i) );
+        temp_img = img_stack(:,:,i);
+        temp_img(temp_perim==1) = max(max(temp_img));
+        figure(1), subplot(1,2,1), imshow( temp_img, [] );
+        subplot(1,2,2), imshow( bw_stack(:,:,i) );
+    end
+end
+
+% placeholder for generating connected components structure
+bw_img = img_stack_to_img_2D(bw_stack, [20 20] );
+cc = bwconncomp(bw_img);
+cc_props = get_cc_regionprops(cc);
+
+keep_idx = [];
+for i = 1:length( cc_props )
+    bnd_box = cc_props(i).BoundingBox;
+    if( cc_props(i).Area>1000 && bnd_box(3)<600 && bnd_box(4)<600  )
+        keep_idx = [keep_idx; i];
+    end
+end
+
+cc_props = cc_props(keep_idx);
+cc.PixelIdxList = cc.PixelIdxList(keep_idx);
+licing_cc.NumObjects = length(keep_idx);
+
+
+
+
+
+
+
+
+
+
+
+
