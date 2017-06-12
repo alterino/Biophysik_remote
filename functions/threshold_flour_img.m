8f830efbf0c66c614e524a49a10f96dec50cd6ec
mjmarino <mjmarino317@gmail.com>
2017-06-12 16:09:23 +0200

    needs to be changed to consider higher signal-to-noise ratios and also (perhaps at a later step) test for confidence of the stripe pattern that was found

--- functions/threshold_flour_img.m
+++ functions/threshold_flour_img.m
@@ -1,19 +1,18 @@
 function [imgbw, conv_hull] = threshold_flour_img( img, size_thresh )
 %UNTITLED2 Summary of this function goes here
 %   Detailed explanation goes here
-
 threshInt = multithresh(img,2);
 
 imgbw = imbinarize(img,max(threshInt));
 
 % linear structure elements used for dilation
-se90 = strel('line', 3, 90);
-se0 = strel('line', 3, 0);
+se90 = strel('line', 1, 90);
+se0 = strel('line', 1, 0);
 % diamond shape used for erosion to compensate for original dilation
 seD = strel('diamond',1);
 
 imgbw_D = imdilate(imgbw, [se90 se0]);
-imgbw_F = imfill(imgbw_D, 'holes');
+% imgbw_F = imfill(imgbw_D, 'holes');
 
 bwC_F = imerode(imgbw_F, seD);
 bwC_F = imerode(bwC_F, seD);
