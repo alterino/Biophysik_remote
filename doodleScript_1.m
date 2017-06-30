60c9672f8134ce63c0273ede6a1db189b7810a58
mjmarino <mjmarino317@gmail.com>
2017-06-30 16:04:43 +0200

    commit for push

--- doodleScript_1.m
+++ doodleScript_1.m
@@ -18,7 +18,7 @@
 % 
 % 
 % fluor_stack = img_2D_to_img_stack(fluor_img, [600 600] );
-% 
+%  
 % bw_stack = zeros( size( fluor_stack ) );
 % cc = cell( size(fluor_stack, 3), 1 );
 % 
@@ -29,18 +29,17 @@
 % end
 % 
 % 
-
-dic_stack = img_2D_to_img_stack( dic_scan, [600 600] );
-label_stack = img_2D_to_img_stack( label_img, [600 600] );
-bw_stack = img_2D_to_img_stack( bw_img, [600 600] );
-entropy_stack = img_2D_to_img_stack( entropy_img, [600 600] );
-
 figure(1)
-for i = 1:size( dic_stack, 3 )
-    
-    subplot(1,3,1), imshow( bw_stack(:,:,i), [] );
-    subplot(1,3,2), imshow( dic_stack(:,:,i), [] );
-    subplot(1,3,3), imshow( entropy_stack(:,:,i), [] );
-    
-    
+for i = 1:length( stats )
+   bb_center = [stats(i).BoundingBox(1) + stats(i).BoundingBox(3)/2,...
+                stats(i).BoundingBox(2) + stats(i).BoundingBox(4)/2];
+            
+   subimg_topleft = [ min( [ max( [bb_center(1)-300, 1] ), size(dic_scan, 2 )-599 ] ) ,...
+                        min( [ max( [bb_center(2)-299, 1] ), size(dic_scan, 1)-599 ] ) ];
+   subimg = dic_scan( subimg_topleft(2):subimg_topleft(2)+599,...
+                        subimg_topleft(1):subimg_topleft(1)+599 );
+   imshow( subimg, [] ), title( sprintf('i = %i', i ) );
+   pause
 end
+
+
--- functions/run_full_eval.m
+++ functions/run_full_eval.m
@@ -1,12 +1,40 @@
-function [ bw_img, label_img, entropy_img ] = run_full_eval( dic_scan, fluor_scan, img_dims, gmm, wind )
+function [ bw_dic, entropy_img, cc, stats ] = run_full_eval( dic_scan, fluor_scan, img_dims, gmm, wind )
 %RUN_FULL_EVAL Summary of this function goes here
 %   Detailed explanation goes here
 
+if( length( size(dic_scan) ) > 2 )
+    error('dic_scan should be a 2D image')
+end
 
-
-[bw_img, label_img, cc, stats, entropy_img] = ...
+[bw_dic, cc, stats, entropy_img] = ...
     process_and_label_DIC( dic_scan, img_dims, wind, gmm  );
 
+keep_idx = false( length(stats), 1 );
+for i = 1:length(stats)
+    if( ~(stats(i).BoundingBox(3) > img_dims(2) ||...
+                stats(i).BoundingBox(4) > img_dims(1) ) )
+        keep_idx(i) = true;
+    end
+end
+
+not_kept_stats = stats( ~(keep_idx) );
+not_kept_cc = cc;
+not_kept_cc.PixelIdxList = not_kept_cc.PixelIdxList{~keep_idx};
+not_kept_cc.PixelIdxList = length( find( keep_idx == false ) );
+
+stats = stats(keep_idx);
+cc.PixelIdxList = cc.PixelIdxList{keep_idx};
+cc.NumObjects = length( find( keep_idx == true ) );
+
+dic_imgs = zeros( img_dims(1), img_dims(2), length( stats ) );
+fluor_imgs = zeros( img_dims(1), img_dims(2), length( stats ) );
+
+for i = 1:length( stats )
+    [rows, cols] = ...
+        compute_idx_from_bound_box( stats(i).BoundingBox, img_dims, size(dic_scan)  );
+    
+    dic_imgs(:,:,i) = extract_sub_img( dic_scan, rows, cols );
+    fluor_imgs(:,:,i) = extract_sub_img( fluor_scan, rows, cols );
 end
 
 
@@ -16,5 +44,4 @@ end
 
 
 
-
-
+end
