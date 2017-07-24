3a52f57b8bf58f8ebf0e27e192f87a2f8227bfb8
mjmarino317 <mjmarino317@gmail.com>
2017-07-24 04:02:14 +0200

    first attempt at autofocus routines completed but not tested

--- CoverslideScanner/v0/CoverslideScanner.m
+++ CoverslideScanner/v0/CoverslideScanner.m
@@ -20,7 +20,8 @@ classdef CoverslideScanner < handle
         Acq = struct(...
             'ScanSize',10,...
             'LaserWvlnth',405,...
-            'z_fluor_est', []) %[nm]
+            'z_fluor_est', [],...
+            'z_shift', []) %[nm]
         
         Analysis = struct( ...
             'DIC', struct('bw_img', [],...
@@ -812,7 +813,7 @@ classdef CoverslideScanner < handle
                
                 z = z0 + zdiff;
                 % should probably set laser power to low here
-                set_objective_stage_z_position_micron(this.MICMAN,z0) 
+                set_objective_stage_z_position_micron(this.MICMAN,z) 
                 [img,~] = snap_img_fluorescence(this.MICMAN,laser);
                 grad_vec(i) = mean( imgradient( img ) );
             end
@@ -820,13 +821,13 @@ classdef CoverslideScanner < handle
             this.z_fluor_est = z0 + z_diff( grad_vec == max(grad_vec) );
         end
         
-        function get_z_optimal_plane_iterative(this)
+        function get_z_optimal_plane_iterative(this, num_iterations)
             
-            z0 = this.z_fluor_est;
-            inc = -.8:.2:.8;
-            grad_vec = zeros( 3, 1 );
-            end_flag = 0;
-            while(~end_flag)
+            z0 = this.Acq.z_fluor_est;
+            inc = -.8:.4:.8;
+            grad_vec = zeros( length(inc), 1 );
+            done_flag = 0;
+            while(~done_flag)
                 for i = length(inc)
                     z = z0 + inc(i);
                     set_objective_stage_z_position_micron(this.MICMAN,z);
@@ -837,21 +838,65 @@ classdef CoverslideScanner < handle
                 opt_idx = find( grad_vec == max( grad_vec ) );
                 if( opt_idx == 1 || opt_idx == length( inc ) )
                     z0 = z0 + inc(opt_idx);
+                    grad_vec = zeros( length(inc), 1 );
                     continue
                 else
                    inc = inc( opt_idx-1:opt_idx+1 );
                    if( inc(1) > inc(3) )
                        inc = inc(1:2);
                    else
-                       inc = inc(2:3);
+                       inc = inc(2:3); 
+                   end
+                   done_flag = 1;
                 end
             end
             
-            
-            
-            
+            for zz = 1:num_iterations
+                inc = [inc(1), inc(1) + .5*(inc(3)-inc(1)), inc(3)];
+                inc = sort(inc);
+                for i = length(inc)
+                    z = z0 + inc(i);
+                    set_objective_stage_z_position_micron(this.MICMAN,z);
+                    % should set laser power to low here
+                    [img,~] = snap_img_fluorescence(this.MICMAN,laser);             
+                    grad_vec(i) = mean( imgradient( img ) );
+                end
+%                 opt_idx = find( grad_vec == max( grad_vec ) );
+                [~, sorted_idx] = sort( grad_vec, 'descend' );
+                inc = sort( inc( sorted_idx(1:2) ) ); % to keep z increment
+                                                      % in ascending order
+                if( diff(inc) <= 0.1 )
+                    continue
+                end
             end
+            
+            z_shift_est = mean( inc );
+            append_z_shift_vec( this, z_shift_est );            
+            
+        end
         
+        
+        function get_z_optimal_plane_polyfit(this)
+            
+            z0 = this.Acq.z_fluor_est;
+            inc = -.8:.4:.8;
+            inc_order = [3 4 2 1 5]; % trying to choose acquisition order
+            inc_order_inv = [4 3 1 2 5];% to optimally account for bleaching
+%             inc = inc( inc_order ); 
+            grad_vec = zeros( 3, 1 );
+            for i = length(inc)
+                z = z0 + inc(inc_order(i));
+                set_objective_stage_z_position_micron(this.MICMAN,z);
+                % should set laser power to low here
+                [img,~] = snap_img_fluorescence(this.MICMAN,laser);             
+                grad_vec(inc_order_inv(i)) = mean( imgradient( img ) );
+            end
+
+            p = polyfit( inc, grad_vec, 2 );
+            z_shift_est = -p(2)/(2*p(1)); % analytical maximum based on polynomial
+
+            append_z_shift_vec( this, z_shift_est );
+
         end
         
         %% setter
@@ -1057,6 +1102,7 @@ classdef CoverslideScanner < handle
             this.Analysis.Fluorescence.stats(idx).stripe_width = width_guess;
             this.Analysis.Fluorescence.stats(idx).num_stripes = num_stripes;
         end
+        
         %% updater
         function update_navi_focus(this,pos)
             set(this.GUI.hAxFocus,...
@@ -1211,6 +1257,10 @@ classdef CoverslideScanner < handle
                 this.GUI.hEditZdcSearchRange],...
                 'Enable','on')
         end %fun
+        
+        function append_z_shift_vec( this, z_shift )
+           this.Acq.z_shift = cat( 1,  this.Acq.z_shift, z_shift ); 
+        end
         
         %%
         function set_zoom(this,src)
---
+++ scripts/autofocus_analysis_script_170723.m
@@ -0,0 +1,36 @@
+
+% load('D:\OS_Biophysik\Microscopy\data\autofocustest4.mat')
+% load('D:\OS_Biophysik\Microscopy\data\autofocustest5.mat')
+
+
+
+
+for i = 1:size( imgs, 2 )
+    for j = 1:size( imgs, 1 )
+        
+        
+        %         var_vec = var_cell{j,i};
+        grad_vec = grad_cell{j,i};
+        img_stack = imgs{j,i};
+        
+        if( isempty( grad_vec ) )
+            continue
+        end
+        
+        %         figure(1), subplot(1,2,1), hold off, plot(  zPosition_vec, var_vec, 'b-' ), grid on
+        figure(1), hold off, plot(  zPosition_vec, grad_vec, 'b-' ), grid on
+        
+        opt_grad_idx = find( grad_vec == max( grad_vec ) );
+        %         opt_var_idx = find( var_vec == max( var_vec ) );
+        
+        %         temp_img = [img_stack(:,:,opt_grad_idx), img_stack(:,:,26),...
+        %                     img_stack(:,:,opt_var_idx)];
+        temp_img = [img_stack(:,:,opt_grad_idx), img_stack(:,:,26)];
+        figure(2), imshow( temp_img, [] )
+%         title( sprintf( 'gradient-%i,    default-26,     variance-%i',...
+%             opt_grad_idx, opt_var_idx ) )
+        title( sprintf( 'gradient-%i,    default-26',...
+            opt_grad_idx) )
+        
+    end
+end
