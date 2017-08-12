400f9ff0aaf82ba110078dbb32b18c277075b1fc
mjmarino317 <mjmarino317@gmail.com>
2017-08-11 20:08:13 -0400

    commit for push

--- scripts/doodleScript.m
+++ scripts/doodleScript.m
@@ -13,20 +13,38 @@
 % end
 
 % imgs_iterative = imgs;
+% 
+% for i = 1:length( imgs_polyfit )
+%    
+%     temp_imgs = imgs_polyfit{i};
+%     grad_vec = zeros( size( temp_imgs, 3), 1 );
+%     
+%     for j = 1:size(temp_imgs, 3)
+%         
+%        figure(1), imshow( temp_imgs(:,:,j), [] )
+%        grad_vec(j) = mean( mean( imgradient( temp_imgs(:,:,j) ) ) );
+%        pause(1)
+%         
+%     end
+%     
+%     figure(2), plot( z0-1.5:.5:z0+1.5, grad_vec(1:end-1), 'b-' );
+%     
+% end
 
-for i = 1:length( imgs_polyfit )
-   
-    temp_imgs = imgs_polyfit{i};
-    grad_vec = zeros( size( temp_imgs, 3), 1 );
-    
-    for j = 1:size(temp_imgs, 3)
-        
-       figure(1), imshow( temp_imgs(:,:,j), [] )
-       grad_vec(j) = mean( mean( imgradient( temp_imgs(:,:,j) ) ) );
-       pause(1)
-        
-    end
-    
-    figure(2), plot( z0-1.5:.5:z0+1.5, grad_vec(1:end-1), 'b-' );
-    
-end
+figure(1), imagesc( corr_radon, 'XData', thetaD + angle_vec, 'YData', -426:426  );
+ylabel('position'); xlabel('theta (degrees)')
+title('Radon Transform of Correlation Image')
+figure(2), subplot(2,2,1), hold off, plot( xp, sum_normalized, 'g-'), title( 'sum normalized' ), grid on;
+hold on, plot( relative_maxima, y_pts1, 'r*' )
+figure(2), subplot(2,2,2), hold off, plot( 1:length(dy_dx), dy_dx, 'g-'), title('derivative of sum normalized'), grid on;
+hold on, plot( find( zero_crossings < 0 ), y_pts2, 'r*' )
+figure(2), subplot( 2,2,3), hold off, plot( maxima_idx, sum_derivatives, 'r*' ), grid on, title('derivative sums')
+xlim([1, length(zero_crossings)]);
+figure(2), subplot(2,2,4), imagesc( img ), title('image with center points')
+hold on, plot( x, y, 'r*' );
+
+figure(3), subplot(2,1,1), hold off, plot( xp, sum_normalized, 'g-'), title( 'Normalized Sum Over All Theta' ), grid on;
+hold on, plot( relative_maxima, y_pts1, 'r*' )
+figure(3), subplot(2,1,2), imshow( img, [] ), title('image with labeled center points')
+hold on, plot( x, y, 'r*' );
+
--- scripts/fluorescence_development_script.m
+++ scripts/fluorescence_development_script.m
@@ -15,32 +15,38 @@
 %     
 % end
 % 
-
-dic_scan = imread( 'D:\OS_Biophysik\Microscopy\170706\DIC_170706_1602.tif' );
-fluor_scan = imread( 'D:\OS_Biophysik\Microscopy\170706\Fluor_405_170706_1510.tif' );
-old_dims = [1200 1200];
-
-scanner = CoverslideScanner;
-close all
-test_eval( scanner, dic_scan, fluor_scan, old_dims, 0 );
+% if( ~exist( 'dic_scan', 'var' ) )
+%     dic_scan = imread( 'D:\OS_Biophysik\Microscopy\170706\DIC_170706_1602.tif' );
+% end
+% if( ~exist( 'fluor_scan', 'var' ) )
+%     fluor_scan = imread( 'D:\OS_Biophysik\Microscopy\170706\Fluor_405_170706_1510.tif' );
+% end
+% old_dims = [1200 1200];
 % 
-[~,sorted_idx_score] = sort( [scanner.Analysis.Fluorescence.stats.score_rank] );
-[~,sorted_idx_mean] = sort( [scanner.Analysis.Fluorescence.stats.mean_rank] );
-sorted_idx_score = sorted_idx_score';
-sorted_idx_mean = sorted_idx_mean';
-
-fluor_stack = scanner.Analysis.Fluorescence.img_stack;
-dic_stack = scanner.Analysis.DIC.img_stack;
-bw_dic = scanner.Analysis.DIC.bw_stack_eval;
-bw_fluor = scanner.Analysis.Fluorescence.bw_stack;
-stats = scanner.Analysis.Fluorescence.stats;
+% scanner = CoverslideScanner;
+% close all
+% test_eval( scanner, dic_scan, fluor_scan, old_dims, 0 );
+% % 
+% [~,sorted_idx_score] = sort( [scanner.Analysis.Fluorescence.stats.score_rank] );
+% [~,sorted_idx_mean] = sort( [scanner.Analysis.Fluorescence.stats.mean_rank] );
+% sorted_idx_score = sorted_idx_score';
+% sorted_idx_mean = sorted_idx_mean';
+% 
+% fluor_stack = scanner.Analysis.Fluorescence.img_stack;
+% dic_stack = scanner.Analysis.DIC.img_stack;
+% bw_dic = scanner.Analysis.DIC.bw_stack_eval;
+% bw_fluor = scanner.Analysis.Fluorescence.bw_stack;
+% stats = scanner.Analysis.Fluorescence.stats;
 
 for i = 1:length( [scanner.Analysis.Fluorescence.stats] )
-    figure(1), subplot(2,2,1), imshow( fluor_stack(:,:,sorted_idx_score(i) ), [] );
-    title( sprintf( 'score = %.5f, thetaD = %.2f, rank = %i',  stats(sorted_idx_score(i)).score,...
-        stats(sorted_idx_score(i)).theta, i ) );
-    subplot(2,2,2), imshow( dic_stack(:,:,sorted_idx_score(i) ), [] );
-    title( sprintf( 'axis length = %.2f', scanner.Analysis.DIC.stack_cc.stats(i).MajorAxisLength ) );
-    subplot(2,2,3), imshow( bw_fluor(:,:,sorted_idx_score(i) ), [] );
-    subplot(2,2,4), imshow( bw_dic(:,:,sorted_idx_score(i) ), [] );
+    temp_fluor = fluor_stack(:,:,sorted_idx_score(i) );
+    temp_dic = dic_stack(:,:,sorted_idx_score(i) );
+    temp_fluor( bwperim( bw_fluor(:,:,sorted_idx_score(i)) ) ) = max( temp_fluor(:) );
+    temp_dic( bwperim( bw_dic(:,:,sorted_idx_score(i)) ) ) = max( temp_dic(:) );
+    figure(1), subplot(1,2,1), imshow( temp_fluor, [] );
+    title( sprintf( 'score = %.5f, rank = %i',  stats(sorted_idx_score(i)).score, i ) );
+    subplot(1,2,2), imshow( temp_dic, [] );
+    title( 'Segmented DIC image' );
+%     subplot(2,2,3), imshow( bw_fluor(:,:,sorted_idx_score(i) ), [] );
+%     subplot(2,2,4), imshow( bw_dic(:,:,sorted_idx_score(i) ), [] );
 end
